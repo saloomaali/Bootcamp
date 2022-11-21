d77127e95895dfc6157001120706ac30dce17a44
Salooma <saloomaali2000@gmail.com>
2022-11-21 11:02:40 +0530

    modified

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Select the operation\n 1 - Addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division5");
+        Scanner obj3 = new Scanner(System.in);
+        int n = input.nextInt();
         System.out.println("Enter two numbers");
         int num1 = input.nextInt();
         int num2 = input.nextInt();
-        System.out.println("Select the operation\n 1 - Addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division");
-        Scanner obj3 = new Scanner(System.in);
-        int n = input.nextInt();
         int result = 0;
 
         switch (n){
